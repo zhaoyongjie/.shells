03d67893199865ecba90aeaa430bc33aa78af588
Yongjie Zhao <yongjie.zhao@gmail.com>
2019-12-05 22:16:49 +0800

    updates

--- init-stretch.sh
+++ init-stretch.sh
@@ -31,6 +31,8 @@ sudo apt update && sudo apt install -y $APPS
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update && sudo apt install -y docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
 
 # install python
